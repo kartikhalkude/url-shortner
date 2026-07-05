c673319131bbf7c49cdbf28f5d5d4f078ee8351e
kartikhalkude <kartikhalkude@gmail.com>
2026-07-06 00:44:53 +0530

    error middle ware

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,7 +1,8 @@
 export default function errorMiddleware(err, req, res, next) {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  const status = err.status || (String(statusCode).startsWith("4") ? "fail" : "error");
+  const status =
+    err.status || (String(statusCode).startsWith("4") ? "fail" : "error");
 
   res.status(statusCode).json({
     status,
